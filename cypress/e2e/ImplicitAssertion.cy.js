6f085a836fea03df0d13bd63f976dbeff0190a77
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-09 18:05:08 +0530

    InplicitAssertion commit

---
+++ cypress/e2e/ImplicitAssertion.cy.js
@@ -0,0 +1,32 @@
+describe('InplicitAssertion',()=>{
+ it('Assertion',()=>{
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+// cy.url().should('include','orangehrmlive.com')
+// cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+// cy.url().should('contains','orangehrm')
+
+
+// cy.url().should('include','orangehrmlive.com')
+// .should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+// .should('contains','orangehrm')
+
+
+// cy.url().should('include','orangehrmlive.com')
+// .and('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+// .and('contains','orangehrm')
+
+
+// cy.title().should('include','Orange')
+// .and('eq','OrangeHRM')
+// .and('contains','HRM')
+
+// cy.get('.orangehrm-login-branding > img').should('be.visible')
+// .and('exist')
+
+
+// cy.get('[name="username"]').type('Admin')
+// cy.get('[name="username"]').should('have.value','Admin')
+
+ })
+ })
+
