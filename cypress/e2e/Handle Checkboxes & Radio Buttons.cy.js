81965185fc7e8bf8c4a2e68660d6237ec1faa086
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-25 08:14:40 +0530

    Handle Checkboxes & Radio Buttons commit

--- cypress/e2e/Handle Checkboxes & Radio Buttons.cy.js
+++ cypress/e2e/Handle Checkboxes & Radio Buttons.cy.js
@@ -1,16 +1,34 @@
-describe("Cheaking Radio Buttons",()=>{
-it(" Radio Buttons",()=>{
-   cy.visit("https://testautomationpractice.blogspot.com/")
+// describe("Cheaking Radio Buttons",()=>{
+// it(" Radio Buttons",()=>{
+//    cy.visit("https://testautomationpractice.blogspot.com/")
 
-//    Cheaking Box
-   cy.get(':nth-child(4) > [name="gender"]').check().should("be.checked")
-   cy.get(':nth-child(3) > [name="gender"]').should("not.be.checked")
+// //    Cheaking Box
+//    cy.get(':nth-child(4) > [name="gender"]').check().should("be.checked")
+//    cy.get(':nth-child(3) > [name="gender"]').should("not.be.checked")
 
-//    Visibility Test
-   cy.get(':nth-child(4) > [name="gender"]').should("be.visible")
-   cy.get(':nth-child(3) > [name="gender"]').should("be.visible")
+// //    Visibility Test
+//    cy.get(':nth-child(4) > [name="gender"]').should("be.visible")
+//    cy.get(':nth-child(3) > [name="gender"]').should("be.visible")
+
+// })
 
 
-   
-})
-})
+//  it("CHECKING CHECK BOXEX",()=>{
+//     cy.visit("https://testautomationpractice.blogspot.com/")
+
+// //    Cheaking Box
+// cy.get('#monday').check().should("be.checked")
+// cy.get('#monday').uncheck().should("not.be.checked")
+
+// //    Visibility Test
+//   cy.get('#monday').should("be.visible")
+
+// // TO SELECT ALL CHECK BOX
+// cy.get(".form-check-input[type='checkbox']").check().should("be.checked")
+//    cy.get(".form-check-input[type='checkbox']").uncheck().should("not.be.checked")
+
+   // TO SELECT FIRST AND LAST CHECK BOX
+//    cy.get(".form-check-input[type='checkbox']").first().check().should("be.checked")
+//    cy.get(".form-check-input[type='checkbox']").last().check().should("be.checked")
+// })
+// })
